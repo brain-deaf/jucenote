379428ca68f999277b1bfcc64a213c5f977ba004
brain-deaf <patrickoneil.music@gmail.com>
2014-05-12 17:52:15 -0700

    seting up new repo

---
+++ note/Source/instrumentMappingEditor.cpp
@@ -0,0 +1,128 @@
+/*
+  ==============================================================================
+
+    instrumentMappingEditor.cpp
+    Created: 11 May 2014 11:31:53am
+    Author:  patrick
+
+  ==============================================================================
+*/
+
+#include "instrumentMappingEditor.h"
+
+instrumentMappingEditor::instrumentMappingEditor(const String& componentName, Component* Parent)
+: parent(Parent), Viewport(componentName){
+    setViewedComponent(graph = new mappingEditorGraph());
+    
+    graph->width = 1800.0f;
+    graph->height = 450.0f;
+    graph->num_columns = 128;
+    graph->setBounds(0, 0, graph->width, graph->height);
+}
+
+instrumentMappingEditor::~instrumentMappingEditor(){
+    graph = nullptr;
+}
+
+
+
+instrumentMappingEditor::mappingEditorGraph::mappingEditorGraph()
+: Component(), dragged_zone(nullptr){
+    Lasso_Source<Zone*> lasso_source;
+}
+
+instrumentMappingEditor::mappingEditorGraph::~mappingEditorGraph(){
+    for (int i=0; i<zones.size(); i++){
+        delete zones[i];
+    }
+    //lasso_source = nullptr;
+}
+
+void instrumentMappingEditor::mappingEditorGraph::paint(Graphics& g){
+    g.setImageResamplingQuality(Graphics::highResamplingQuality);
+    
+    g.fillAll (Colours::antiquewhite);
+    
+    float grid_outline = 1.0f;
+    float grid_width = width / num_columns;
+    
+    Path myPath;
+    for (int i=0; i<this->num_columns; i++){
+        myPath.startNewSubPath (i*grid_width, 0.0f);       
+        myPath.lineTo (i*grid_width, height);                
+    }
+
+    g.strokePath (myPath, PathStrokeType (grid_outline));
+}
+
+void instrumentMappingEditor::mappingEditorGraph::filesDropped(const StringArray& files, int x, int y){
+    float grid_outline = 1.0f;
+    float grid_width = width / num_columns;
+    
+    for (int i=0; i<files.size(); i++){
+        Zone* new_zone;
+        zones.add(new_zone = new Zone(files[i]));
+        
+        new_zone->removeListener(this);
+        new_zone->addListener(this);
+        new_zone->removeMouseListener(new_zone);
+        new_zone->addMouseListener(this, true);
+
+        new_zone->x = ((int)(x / grid_width)) * grid_width + grid_outline + grid_width*i;
+        new_zone->y = 0;
+        new_zone->height = height;
+        new_zone->register_parent(this);
+        
+        new_zone->setBounds(new_zone->x, new_zone->y, grid_width - grid_outline, new_zone->height);
+        addAndMakeVisible(new_zone);
+        
+        lasso_source.zones.add(new_zone);
+    }
+}
+
+void instrumentMappingEditor::mappingEditorGraph::buttonClicked(Button* button){
+    dragged_zone = (Zone*)button;
+    std::cout<<"dragged zone: "<<dragged_zone;
+}
+
+void instrumentMappingEditor::mappingEditorGraph::mouseDown(const MouseEvent& e){
+    std::cout<<"creating LassoComponent"<<std::endl;
+    lasso = new LassoComponent<Zone*>;
+    std::cout<<"making visible LassoComponent"<<std::endl;
+    addAndMakeVisible(lasso);
+    std::cout<<"begin lasso"<<std::endl;
+    lasso->beginLasso(e, &lasso_source);
+}
+
+void instrumentMappingEditor::mappingEditorGraph::mouseDrag(const MouseEvent& e){
+    if (dragged_zone != nullptr){
+        float grid_outline = 1.0f;
+        float grid_width = width / num_columns;
+        
+        int grid_x_offset = (int)(e.x / grid_width);
+        int grid_x = (int)(dragged_zone->x / grid_width);
+        int new_grid_x = grid_x_offset + grid_x;
+    
+        if (new_grid_x >= 0 and new_grid_x < num_columns){
+            dragged_zone->x = new_grid_x * grid_width + grid_outline; 
+            dragged_zone->setBounds(dragged_zone->x, dragged_zone->y, grid_width - grid_outline, dragged_zone->height);
+        }
+    }else{
+        lasso->dragLasso(e);
+    }
+}
+
+void instrumentMappingEditor::mappingEditorGraph::mouseUp(const MouseEvent& e){
+    dragged_zone = nullptr;
+    lasso->endLasso();
+    lasso = nullptr;
+}
+
+void instrumentMappingEditor::mappingEditorGraph::Zone::register_parent(instrumentMappingEditor::mappingEditorGraph* c){parent=c;}
+
+void instrumentMappingEditor::mappingEditorGraph::Zone::mouseDown(const MouseEvent& e){
+    parent->dragged_zone = this;
+    this->clicked();
+    std::cout<<"button down from zone listener"<<std::endl;
+}
+
---
+++ note/Source/instrumentMappingEditor.h
@@ -0,0 +1,110 @@
+/*
+  ==============================================================================
+
+    instrumentMappingEditor.h
+    Created: 11 May 2014 11:32:11am
+    Author:  patrick
+
+  ==============================================================================
+*/
+
+#ifndef INSTRUMENTMAPPINGEDITOR_H_INCLUDED
+#define INSTRUMENTMAPPINGEDITOR_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class instrumentMappingEditor : public Viewport
+{
+public:
+    class mappingEditorGraph;
+    
+    instrumentMappingEditor(const String& componentName, Component* Parent);
+    ~instrumentMappingEditor();
+private:
+    ScopedPointer<Component> parent;
+    ScopedPointer<mappingEditorGraph> graph;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (instrumentMappingEditor)
+};
+    
+class instrumentMappingEditor::mappingEditorGraph : 
+public Component, public FileDragAndDropTarget, public ButtonListener
+{
+public:
+    float width;
+    float height;
+    int num_columns;
+    
+    class Zone;
+    
+    mappingEditorGraph();
+    ~mappingEditorGraph();
+    
+    void paint(Graphics& g);
+    void buttonClicked(Button* button);
+    
+    void mouseDrag(const MouseEvent& event);
+    void mouseUp(const MouseEvent& event);
+    void mouseDown(const MouseEvent& event);
+    
+    void filesDropped(const StringArray& files, int x, int y);
+    bool isInterestedInFileDrag(const StringArray& files){return true;};
+    
+    Array<Zone*> zones;
+    ScopedPointer<LassoComponent<Zone*> > lasso;
+    
+    template <class SelectableItemType>
+    class Lasso_Source : public LassoSource<SelectableItemType>, public ChangeListener
+    {
+    public:
+        ~Lasso_Source() {set = nullptr;};
+        Array<Zone*> zones;
+        SelectedItemSet<SelectableItemType>* set;
+        
+        void findLassoItemsInArea (Array <SelectableItemType>& itemsFound,
+                                       const Rectangle<int>& area){
+            itemsFound.add(zones[0]);
+            std::cout<<itemsFound.size()<<std::endl;
+        };
+        SelectedItemSet<SelectableItemType>& getLassoSelection(){
+            set = new SelectedItemSet<SelectableItemType>;
+            set->addChangeListener(this);
+            //set->selectOnly(zones[0]);
+            return *set;
+        };
+        
+        void changeListenerCallback(ChangeBroadcaster* source){
+            std::cout<<"changed!"<<std::endl;
+            if (source = set){
+                std::cout<<source->begin()->x;
+            }
+        };
+    };
+
+    
+    Lasso_Source<Zone*> lasso_source;
+private:
+    Zone* dragged_zone;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (mappingEditorGraph)
+};
+
+class instrumentMappingEditor::mappingEditorGraph::Zone : public TextButton
+{
+public:
+    const String name;
+    int x;
+    int y;
+    int height;
+    instrumentMappingEditor::mappingEditorGraph* parent;
+    
+    void register_parent(instrumentMappingEditor::mappingEditorGraph* parent);
+    void mouseDown(const MouseEvent& event);
+
+    Zone(const String& sample_name) : TextButton(""), name(sample_name){};
+    
+private:
+    
+};
+
+
+
+#endif  // INSTRUMENTMAPPINGEDITOR_H_INCLUDED
